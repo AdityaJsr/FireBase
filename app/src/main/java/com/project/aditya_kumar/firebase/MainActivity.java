8997999cfadfb2aec99867cdcb235577ea1c4494
aditya <adityajsr>
2018-12-18 21:50:44 +0530

    gallery access is done

--- app/src/main/java/com/project/aditya_kumar/firebase/MainActivity.java
+++ app/src/main/java/com/project/aditya_kumar/firebase/MainActivity.java
@@ -1,13 +1,43 @@
 package com.project.aditya_kumar.firebase;
 
+import android.content.Intent;
+import android.media.Image;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btn1;
+    private static final int GALLERY_REQUEST = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn1 = findViewById(R.id.btn_1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Select The Image To Upload", Toast.LENGTH_SHORT).show();
+
+                Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                galleryIntent.setType("image/*");
+                startActivityForResult(galleryIntent, GALLERY_REQUEST);
+            }
+        });
+    }
+
+      @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == GALLERY_REQUEST && resultCode == RESULT_OK){
+
+        }
     }
 }
+
